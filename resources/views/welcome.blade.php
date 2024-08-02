2a3ee1b075b1ffb473c205b3ea2b18bcf22c3b42
hasby2345 <mhmmdhasby83@gmail.com>
2024-08-02 14:04:52 +0700

    authh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -169,4 +169,27 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
+
+
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    Selamat Datang @guest
+                        @else
+                        <b>{{Auth::user()->user}}</b>
+                    @endguest
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
